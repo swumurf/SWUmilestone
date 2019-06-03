70165f919e3814acd5bf473a9880079413e132dc
YounaJoo <gkdld299@daum.net>
2019-06-03 21:38:21 +0900

    Camera

--- extest/components/Usercertificate.js
+++ extest/components/Usercertificate.js
@@ -1,70 +1,60 @@
 import React, { Component } from 'react';
-import {StyleSheet,Text,View,Image, CameraRoll,TextInput,
-    TouchableOpacity,Button,AsyncStorage, PermissionsAndroid} from 'react-native';
+import {StyleSheet,Text,View,Image, TouchableOpacity} from 'react-native';
+import {Camera, Permissions} from 'expo';
     
-// async function requestCameraPermission() {
-//     try {
-//         const granted = await PermissionsAndroid.request(
-//             PermissionsAndroid.PERMISSIONS.CAMERA,
-//             {
-//                 title: 'Cool Photo App Camera Permission',
-//                 message:
-//                 'Cool Photo App needs access to your camera ' +
-//                 'so you can take awesome pictures.',
-//                 buttonNeutral: 'Ask Me Later',
-//                 buttonNegative: 'Cancel',
-//                 buttonPositive: 'OK',
-//             },);
-//             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-//                 console.log('You can use the camera');
-//             } else {
-//                 console.log('Camera permission denied');
-//             }
-//         } catch (err) {
-//             console.warn(err);
-//         }
-//     }
-
-// CameraRoll.getPhotos(
-//   {first: 1},
-//   (data) => {
-//       console.log(data);
-//   },
-//   (error) => {
-//       console.warn(error);
-//   }
-// ); 
-
 export default class Usercertificate extends React.Component {
+    state = {
+        hasCameraPermission : null,
+        type : Camera.Constants.Type.back,
+    }
 
-    // constructor(props){
-    //     super(props);
-    //     this.state = {photoSource : null};
-    // }
-
-    // componentDidMount(){
-    //     CameraRoll.getPhotos({first: 1}).then(data => {
-    //         this.setState({photoSource: {uri: data.edges[3].node.image.uri}});
-    //     }, error => {
-    //        console.warn(error);
-    //     }); 
-    // }
+    async componentDidMount(){
+        const {status} = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status == 'granted'});
+    }
 
     render(){
-        return (
-            <View style={styles.container}>
-                <Text>Usercertificate</Text>
-                <Button onPress={() => { this.props.navigation.navigate('Main')}}></Button>
-                
-            {/* <View>
-                <Image 
-                    style = {styles.backdrop}
-                    source = {this.state.photoSource}
-                    resizeMode = "cover"
-                    >{this.props.children}</Image>
-                </View> */}
-            </View>
-        );
+        const {hasCameraPermission} = this.state;
+        if(hasCameraPermission === null){
+            return <View />;
+        }else if(hasCameraPermission === false){
+            return <Text>No access to camera</Text>
+        }else{
+            return(
+                <View style = {{ flex: 1 }}>
+                    <Camera style = {{ flex : 1 }}>
+                        <View
+                            style = {{
+                                flex : 1,
+                                backgroundColor: 'transparent',
+                                flexDirection: 'row',
+                            }}>
+                            <TouchableOpacity
+                                style = {{
+                                    flex: 0.1,
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                }}
+                                
+                                onPress = {() => {
+                                    this.setState({
+                                        type: this.state.type === Camera.container.Type.back
+                                        ? Camera.Constants.Type.front
+                                        : Camera.Constants.Type.back,
+                                    });
+                                }}>
+                                <Text style = {{
+                                    fontSize: 18,
+                                    marginBottom: 10,
+                                    color: 'white' }}>
+                                {' '}Flip{' '}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                </View>
+            );
+        }
     }
 }
 
