aff5293112116d0986e8c99b28ef052dd025d4b0
jean1042 <jean1042@naver.com>
2019-08-20 21:59:05 +0900

    Update major Dropdown list
    Update major Dropdown list 1,2,3

--- extest02/components/Signup.js
+++ extest02/components/Signup.js
@@ -4,11 +4,12 @@ import {StyleSheet,Text,View,Image,TextInput,TouchableOpacity,Button,AsyncStorag
 
 
 export default class Signup extends React.Component {
+  
   constructor(props){
     super(props);
     const {navigation}=this.props;
-    this.state={studentNum:'', password:'',studentName:'',major1:'',major2:'',major3:'',supervisor:'',phoneNum:'',email:'',mileage:'',goal_graduate:''};
-  }  
+    this.state={studentNum:'',password:'',studentName:'',major1:'',major2:'',major3:'',supervisor:'',phoneNum:'',email:'',mileage:'',goal_graduate:''};
+  }
 
   postData=async()=>{
     try{
@@ -74,88 +75,203 @@ export default class Signup extends React.Component {
               <Picker
               selectedValue={this.state.major1}
               style={{height:50,width:'70%'}}
-              onValueChange={(itemValue,itemIndex)=>this.setState({major1:itemValue})}>
+              onValueChange={(itemValue)=>this.setState({major1:itemValue})}>
                 <Picker.Item label="국어국문학과" value="국어국문학과"/>
                 <Picker.Item label="영어영문학과" value="영어영문학과"/>
-                <Picker.Item label="불어불문학과" value="영어영문학과"/>
-                <Picker.Item label="독어독문학과" value="영어영문학과"/>
-                <Picker.Item label="중어중문학과" value="영어영문학과"/>
-                <Picker.Item label="일어일문학과" value="영어영문학과"/>
-                <Picker.Item label="사학과" value="영어영문학과"/>
-                <Picker.Item label="기독교학과" value="영어영문학과"/>
-                <Picker.Item label="문예창작전공" value="영어영문학과"/>
-                <Picker.Item label="박물관학전공" value="영어영문학과"/>
-                <Picker.Item label="글로벌문화산업/MICE전공" value="영어영문학과"/>
-                <Picker.Item label="스마트농업공학전공" value="영어영문학과"/>
-                <Picker.Item label="바이오인포매틱스전공" value="영어영문학과"/>
-                <Picker.Item label="바이오화장품공학전공" value="영어영문학과"/>
-                <Picker.Item label="경제학과" value="영어영문학과"/>
-                <Picker.Item label="문헌정보학과" value="영어영문학과"/>
-                <Picker.Item label="청소년학전공" value="영어영문학과"/>
-                <Picker.Item label="노년학전공" value="영어영문학과"/>
-                <Picker.Item label="사회복지학과" value="영어영문학과"/>
-                <Picker.Item label="아동학과" value="영어영문학과"/>
-                <Picker.Item label="행정학과" value="영어영문학과"/>
-                <Picker.Item label="언론영상학과" value="영어영문학과"/>
-                <Picker.Item label="저널리즘전공" value="영어영문학과"/>
-                <Picker.Item label="비즈니스커뮤니케이션전공" value="영어영문학과"/>
-                <Picker.Item label="디지털영상전공" value="영어영문학과"/>
-                <Picker.Item label="국제학전공" value="영어영문학과"/>
-                <Picker.Item label="교육심리학과" value="영어영문학과"/>
-                <Picker.Item label="체육학과" value="영어영문학과"/>
-                <Picker.Item label="기업보안융합전공" value="영어영문학과"/>
-                <Picker.Item label="데이터과학전공" value="영어영문학과"/>
-                <Picker.Item label="휴먼서비스전공" value="영어영문학과"/>
-                <Picker.Item label="공공안전전공" value="영어영문학과"/>
-                <Picker.Item label="스마트헬스케어전공" value="영어영문학과"/>
-                <Picker.Item label="디지털융합경영전공" value="영어영문학과"/>
-                <Picker.Item label="수학과" value="영어영문학과"/>
-                <Picker.Item label="화학생명환경과학부" value="영어영문학과"/>
-                <Picker.Item label="화학과" value="영어영문학과"/>
-                <Picker.Item label="생명환경공학전공" value="영어영문학과"/>
-                <Picker.Item label="생명환경공학과" value="영어영문학과"/>
-                <Picker.Item label="원예생명조경학과" value="영어영문학과"/>
-                <Picker.Item label="식품응용시스템학부" value="영어영문학과"/>
-                <Picker.Item label="식품공학과" value="영어영문학과"/>
-                <Picker.Item label="식품영양학과" value="영어영문학과"/>
-                <Picker.Item label="도시환경예술디자인전공" value="영어영문학과"/>
-                <Picker.Item label="패션디자인/비즈니스 전공" value="영어영문학과"/>
-                <Picker.Item label="사회원예학전공" value="영어영문학과"/>
-                <Picker.Item label="경영학과" value="영어영문학과"/>
-                <Picker.Item label="의류학과" value="영어영문학과"/>
-                <Picker.Item label="패션산업학과" value="영어영문학과"/>
-                <Picker.Item label="디지털미디어학과" value="영어영문학과"/>
-                <Picker.Item label="정보보호학과" value="영어영문학과"/>
-                <Picker.Item label="소프트웨어융합학과" value="영어영문학과"/>
-                <Picker.Item label="멀티미디어학과" value="영어영문학과"/>
-                <Picker.Item label="산업디자인학과" value="영어영문학과"/>
-                <Picker.Item label="서양화과" value="영어영문학과"/>
-                <Picker.Item label="현대미술전공" value="영어영문학과"/>
-                <Picker.Item label="공예전공" value="영어영문학과"/>
-                <Picker.Item label="공예학과" value="영어영문학과"/>
-                <Picker.Item label="시각디자인학과" value="영어영문학과"/>
-                
-
-                
-                
-
-
-
-                
-              </Picker>
-              {/* <TextInput style={styles.input} name="major1" placeholder="제1전공을 입력해주세요" placeholderTextColor="#99A3A4" 
-                onChangeText={(text)=>this.setState({major1:text})}></TextInput>*/}
+                <Picker.Item label="불어불문학과" value="불어불문학과"/>
+                <Picker.Item label="독어독문학과" value="독어독문학과"/>
+                <Picker.Item label="중어중문학과" value="중어중문학과"/>
+                <Picker.Item label="일어일문학과" value="일어일문학과"/>
+                <Picker.Item label="사학과" value="사학과"/>
+                <Picker.Item label="기독교학과" value="기독교학과"/>
+                <Picker.Item label="문예창작전공" value="문예창작전공"/>
+                <Picker.Item label="박물관학전공" value="박물관학전공"/>
+                <Picker.Item label="글로벌문화산업/MICE전공" value="글로벌문화산업/MICE전공"/>
+                <Picker.Item label="스마트농업공학전공" value="스마트농업공학전공"/>
+                <Picker.Item label="바이오인포매틱스전공" value="바이오인포매틱스전공"/>
+                <Picker.Item label="바이오화장품공학전공" value="바이오화장품공학전공"/>
+                <Picker.Item label="경제학과" value="경제학과"/>
+                <Picker.Item label="문헌정보학과" value="문헌정보학과"/>
+                <Picker.Item label="청소년학전공" value="청소년학전공"/>
+                <Picker.Item label="노년학전공" value="노년학전공"/>
+                <Picker.Item label="사회복지학과" value="사회복지학과"/>
+                <Picker.Item label="아동학과" value="아동학과"/>
+                <Picker.Item label="행정학과" value="행정학과"/>
+                <Picker.Item label="언론영상학과" value="언론영상학과"/>
+                <Picker.Item label="저널리즘전공" value="저널리즘전공"/>
+                <Picker.Item label="비즈니스커뮤니케이션전공" value="비즈니스커뮤니케이션전공"/>
+                <Picker.Item label="디지털영상전공" value="디지털영상전공"/>
+                <Picker.Item label="국제학전공" value="국제학전공"/>
+                <Picker.Item label="교육심리학과" value="교육심리학과"/>
+                <Picker.Item label="체육학과" value="체육학과"/>
+                <Picker.Item label="기업보안융합전공" value="기업보안융합전공"/>
+                <Picker.Item label="데이터과학전공" value="데이터과학전공"/>
+                <Picker.Item label="휴먼서비스전공" value="휴먼서비스전공"/>
+                <Picker.Item label="공공안전전공" value="공공안전전공"/>
+                <Picker.Item label="스마트헬스케어전공" value="스마트헬스케어전공"/>
+                <Picker.Item label="디지털융합경영전공" value="디지털융합경영전공"/>
+                <Picker.Item label="수학과" value="수학과"/>
+                <Picker.Item label="화학생명환경과학부" value="화학생명환경과학부"/>
+                <Picker.Item label="화학과" value="화학과"/>
+                <Picker.Item label="생명환경공학전공" value="생명환경공학전공"/>
+                <Picker.Item label="생명환경공학과" value="생명환경공학과"/>
+                <Picker.Item label="원예생명조경학과" value="원예생명조경학과"/>
+                <Picker.Item label="식품응용시스템학부" value="식품응용시스템학부"/>
+                <Picker.Item label="식품공학과" value="식품공학과"/>
+                <Picker.Item label="식품영양학과" value="식품영양학과"/>
+                <Picker.Item label="도시환경예술디자인전공" value="도시환경예술디자인전공"/>
+                <Picker.Item label="패션디자인/비즈니스 전공" value="패션디자인/비즈니스 전공"/>
+                <Picker.Item label="사회원예학전공" value="사회원예학전공"/>
+                <Picker.Item label="경영학과" value="경영학과"/>
+                <Picker.Item label="의류학과" value="의류학과"/>
+                <Picker.Item label="패션산업학과" value="패션산업학과"/>
+                <Picker.Item label="디지털미디어학과" value="디지털미디어학과"/>
+                <Picker.Item label="정보보호학과" value="정보보호학과"/>
+                <Picker.Item label="소프트웨어융합학과" value="소프트웨어융합학과"/>
+                <Picker.Item label="멀티미디어학과" value="멀티미디어학과"/>
+                <Picker.Item label="산업디자인학과" value="산업디자인학과"/>
+                <Picker.Item label="서양화과" value="서양화과"/>
+                <Picker.Item label="현대미술전공" value="현대미술전공"/>
+                <Picker.Item label="공예전공" value="공예전공"/>
+                <Picker.Item label="공예학과" value="공예학과"/>
+                <Picker.Item label="시각디자인학과" value="시각디자인학과"/>
+                </Picker>
             </View> 
 
             <View style = {styles.logRow}>
               <Text style={styles.logText}>  제2전공 </Text>
-              <TextInput style={styles.input} name="major2" placeholder="제2전공을 입력해주세요" placeholderTextColor="#99A3A4" 
-                onChangeText={(text)=>this.setState({major2:text})}></TextInput>
+              <Picker
+               selectedValue={this.state.major2}
+              style={{height:50,width:'70%'}}
+              onValueChange={(itemValue)=>this.setState({major2:itemValue})}>
+                <Picker.Item label="국어국문학과" value="국어국문학과"/>
+                <Picker.Item label="영어영문학과" value="영어영문학과"/>
+                <Picker.Item label="불어불문학과" value="불어불문학과"/>
+                <Picker.Item label="독어독문학과" value="독어독문학과"/>
+                <Picker.Item label="중어중문학과" value="중어중문학과"/>
+                <Picker.Item label="일어일문학과" value="일어일문학과"/>
+                <Picker.Item label="사학과" value="사학과"/>
+                <Picker.Item label="기독교학과" value="기독교학과"/>
+                <Picker.Item label="문예창작전공" value="문예창작전공"/>
+                <Picker.Item label="박물관학전공" value="박물관학전공"/>
+                <Picker.Item label="글로벌문화산업/MICE전공" value="글로벌문화산업/MICE전공"/>
+                <Picker.Item label="스마트농업공학전공" value="스마트농업공학전공"/>
+                <Picker.Item label="바이오인포매틱스전공" value="바이오인포매틱스전공"/>
+                <Picker.Item label="바이오화장품공학전공" value="바이오화장품공학전공"/>
+                <Picker.Item label="경제학과" value="경제학과"/>
+                <Picker.Item label="문헌정보학과" value="문헌정보학과"/>
+                <Picker.Item label="청소년학전공" value="청소년학전공"/>
+                <Picker.Item label="노년학전공" value="노년학전공"/>
+                <Picker.Item label="사회복지학과" value="사회복지학과"/>
+                <Picker.Item label="아동학과" value="아동학과"/>
+                <Picker.Item label="행정학과" value="행정학과"/>
+                <Picker.Item label="언론영상학과" value="언론영상학과"/>
+                <Picker.Item label="저널리즘전공" value="저널리즘전공"/>
+                <Picker.Item label="비즈니스커뮤니케이션전공" value="비즈니스커뮤니케이션전공"/>
+                <Picker.Item label="디지털영상전공" value="디지털영상전공"/>
+                <Picker.Item label="국제학전공" value="국제학전공"/>
+                <Picker.Item label="교육심리학과" value="교육심리학과"/>
+                <Picker.Item label="체육학과" value="체육학과"/>
+                <Picker.Item label="기업보안융합전공" value="기업보안융합전공"/>
+                <Picker.Item label="데이터과학전공" value="데이터과학전공"/>
+                <Picker.Item label="휴먼서비스전공" value="휴먼서비스전공"/>
+                <Picker.Item label="공공안전전공" value="공공안전전공"/>
+                <Picker.Item label="스마트헬스케어전공" value="스마트헬스케어전공"/>
+                <Picker.Item label="디지털융합경영전공" value="디지털융합경영전공"/>
+                <Picker.Item label="수학과" value="수학과"/>
+                <Picker.Item label="화학생명환경과학부" value="화학생명환경과학부"/>
+                <Picker.Item label="화학과" value="화학과"/>
+                <Picker.Item label="생명환경공학전공" value="생명환경공학전공"/>
+                <Picker.Item label="생명환경공학과" value="생명환경공학과"/>
+                <Picker.Item label="원예생명조경학과" value="원예생명조경학과"/>
+                <Picker.Item label="식품응용시스템학부" value="식품응용시스템학부"/>
+                <Picker.Item label="식품공학과" value="식품공학과"/>
+                <Picker.Item label="식품영양학과" value="식품영양학과"/>
+                <Picker.Item label="도시환경예술디자인전공" value="도시환경예술디자인전공"/>
+                <Picker.Item label="패션디자인/비즈니스 전공" value="패션디자인/비즈니스 전공"/>
+                <Picker.Item label="사회원예학전공" value="사회원예학전공"/>
+                <Picker.Item label="경영학과" value="경영학과"/>
+                <Picker.Item label="의류학과" value="의류학과"/>
+                <Picker.Item label="패션산업학과" value="패션산업학과"/>
+                <Picker.Item label="디지털미디어학과" value="디지털미디어학과"/>
+                <Picker.Item label="정보보호학과" value="정보보호학과"/>
+                <Picker.Item label="소프트웨어융합학과" value="소프트웨어융합학과"/>
+                <Picker.Item label="멀티미디어학과" value="멀티미디어학과"/>
+                <Picker.Item label="산업디자인학과" value="산업디자인학과"/>
+                <Picker.Item label="서양화과" value="서양화과"/>
+                <Picker.Item label="현대미술전공" value="현대미술전공"/>
+                <Picker.Item label="공예전공" value="공예전공"/>
+                <Picker.Item label="공예학과" value="공예학과"/>
+                <Picker.Item label="시각디자인학과" value="시각디자인학과"/>
+                </Picker>
             </View>
+
             <View style = {styles.logRow}>
               <Text style={styles.logText}>  제3전공 </Text>
-              <TextInput style={styles.input} name="major3" placeholder="제3전공을 입력해주세요" placeholderTextColor="#99A3A4" 
-                onChangeText={(text)=>this.setState({major3:text})}></TextInput>
+              <Picker
+               selectedValue={this.state.major3}
+                style={{height:50,width:'70%'}}
+                 onValueChange={(itemValue)=>this.setState({major3:itemValue})}>
+                <Picker.Item label="국어국문학과" value="국어국문학과"/>
+                <Picker.Item label="영어영문학과" value="영어영문학과"/>
+                <Picker.Item label="불어불문학과" value="불어불문학과"/>
+                <Picker.Item label="독어독문학과" value="독어독문학과"/>
+                <Picker.Item label="중어중문학과" value="중어중문학과"/>
+                <Picker.Item label="일어일문학과" value="일어일문학과"/>
+                <Picker.Item label="사학과" value="사학과"/>
+                <Picker.Item label="기독교학과" value="기독교학과"/>
+                <Picker.Item label="문예창작전공" value="문예창작전공"/>
+                <Picker.Item label="박물관학전공" value="박물관학전공"/>
+                <Picker.Item label="글로벌문화산업/MICE전공" value="글로벌문화산업/MICE전공"/>
+                <Picker.Item label="스마트농업공학전공" value="스마트농업공학전공"/>
+                <Picker.Item label="바이오인포매틱스전공" value="바이오인포매틱스전공"/>
+                <Picker.Item label="바이오화장품공학전공" value="바이오화장품공학전공"/>
+                <Picker.Item label="경제학과" value="경제학과"/>
+                <Picker.Item label="문헌정보학과" value="문헌정보학과"/>
+                <Picker.Item label="청소년학전공" value="청소년학전공"/>
+                <Picker.Item label="노년학전공" value="노년학전공"/>
+                <Picker.Item label="사회복지학과" value="사회복지학과"/>
+                <Picker.Item label="아동학과" value="아동학과"/>
+                <Picker.Item label="행정학과" value="행정학과"/>
+                <Picker.Item label="언론영상학과" value="언론영상학과"/>
+                <Picker.Item label="저널리즘전공" value="저널리즘전공"/>
+                <Picker.Item label="비즈니스커뮤니케이션전공" value="비즈니스커뮤니케이션전공"/>
+                <Picker.Item label="디지털영상전공" value="디지털영상전공"/>
+                <Picker.Item label="국제학전공" value="국제학전공"/>
+                <Picker.Item label="교육심리학과" value="교육심리학과"/>
+                <Picker.Item label="체육학과" value="체육학과"/>
+                <Picker.Item label="기업보안융합전공" value="기업보안융합전공"/>
+                <Picker.Item label="데이터과학전공" value="데이터과학전공"/>
+                <Picker.Item label="휴먼서비스전공" value="휴먼서비스전공"/>
+                <Picker.Item label="공공안전전공" value="공공안전전공"/>
+                <Picker.Item label="스마트헬스케어전공" value="스마트헬스케어전공"/>
+                <Picker.Item label="디지털융합경영전공" value="디지털융합경영전공"/>
+                <Picker.Item label="수학과" value="수학과"/>
+                <Picker.Item label="화학생명환경과학부" value="화학생명환경과학부"/>
+                <Picker.Item label="화학과" value="화학과"/>
+                <Picker.Item label="생명환경공학전공" value="생명환경공학전공"/>
+                <Picker.Item label="생명환경공학과" value="생명환경공학과"/>
+                <Picker.Item label="원예생명조경학과" value="원예생명조경학과"/>
+                <Picker.Item label="식품응용시스템학부" value="식품응용시스템학부"/>
+                <Picker.Item label="식품공학과" value="식품공학과"/>
+                <Picker.Item label="식품영양학과" value="식품영양학과"/>
+                <Picker.Item label="도시환경예술디자인전공" value="도시환경예술디자인전공"/>
+                <Picker.Item label="패션디자인/비즈니스 전공" value="패션디자인/비즈니스 전공"/>
+                <Picker.Item label="사회원예학전공" value="사회원예학전공"/>
+                <Picker.Item label="경영학과" value="경영학과"/>
+                <Picker.Item label="의류학과" value="의류학과"/>
+                <Picker.Item label="패션산업학과" value="패션산업학과"/>
+                <Picker.Item label="디지털미디어학과" value="디지털미디어학과"/>
+                <Picker.Item label="정보보호학과" value="정보보호학과"/>
+                <Picker.Item label="소프트웨어융합학과" value="소프트웨어융합학과"/>
+                <Picker.Item label="멀티미디어학과" value="멀티미디어학과"/>
+                <Picker.Item label="산업디자인학과" value="산업디자인학과"/>
+                <Picker.Item label="서양화과" value="서양화과"/>
+                <Picker.Item label="현대미술전공" value="현대미술전공"/>
+                <Picker.Item label="공예전공" value="공예전공"/>
+                <Picker.Item label="공예학과" value="공예학과"/>
+                <Picker.Item label="시각디자인학과" value="시각디자인학과"/></Picker>
+            
             </View>
             <View style = {styles.logRow}>
               <Text style={styles.logText}>  지도교수 </Text>
@@ -183,6 +299,9 @@ export default class Signup extends React.Component {
           <View>
             <TouchableOpacity style={{alignItems: 'center'}} onPress={()=>{
               console.log('major1:'+this.state.major1);
+              console.log('major2:'+this.state.major2);
+              console.log('major3:'+this.state.major3);
+              
            // this.postData();
             this.props.navigation.navigate('Mainpage');}}>
               <Text style={styles.managerText}>관리자로 로그인하기</Text>
