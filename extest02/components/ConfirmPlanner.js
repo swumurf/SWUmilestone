13dbde9310dd395c2c95db7d50ebbe9743a304a5
jean1042 <jean1042@naver.com>
2019-11-27 02:04:00 +0900

    전체 플래너 조회 통신0
    전체 플래너 조회 통신0

--- extest02/components/ConfirmPlanner.js
+++ extest02/components/ConfirmPlanner.js
@@ -3,25 +3,79 @@ import {SectionList,FlatList, StyleSheet,Text,View,Image,TextInput,TouchableOpac
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default class ConfirmPlanner extends React.Component {
+
+  static navigationOptions = {
+    header: null
+  }
+  
   constructor(props){
     super(props);
     const {navigation}=this.props;
     this.state={
-      activityYear:navigation.getParam('activityYear'),
-      activityMonth:navigation.getParam('activityMonth'),
-      activityDetailNum:navigation.getParam('activityDetailNum'),
-      startDate:'',expireDate:'',title:''};
+      studentIdx:1,
+      activityYear:'',
+      activityName:'',
+      activityMonth:'',
+      activityDetailNum:'',
+      activityStartDate:'',
+      activityExpireDate:'',
+     evidentialIdx:'',
+     activities:[]
+    };
+    
   }
+  /*
+  handleArrays(str){
+    var newActivities=[];
+    newActivities=(this.state.activities).concat(str);
+
+    this.setState(this.state.activities=newActivities);
+    console.log('newActivities:'+this.state.activities);
+  }
+  */
+ 
+  postData=async()=>{
+    try{
+      console.log('Planner 전체 조회 함수');
+      let url='http://15.165.96.110:3000/planner/planner/'+this.state.studentIdx;
+      console.log('url: '+ url);
+      
+      let res=await fetch('http://15.165.96.110:3000/planner/planner/'+this.state.studentIdx, {
+      
+      method:'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type':'application/json'
+      }
+      });
+      
+      res=await res.json();
+      console.log(res.data.length);
+      var activityNames=new Array();
+      for(var i=0;i<res.data.length;i++){
+       activityNames.push(res.data[i].activityName);
+       //this.handleArrays(res.data[i].activityName);
+        console.log(activityNames[i]);
+      }
+
   
+    
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+
   render(){
+    this.postData();
   return (
     <View styles={styles.container}>
        <SectionList
           sections={[
-            {title: '1학년', data: ['Devin', 'Dan', 'Dominic']},
-            {title: '2학년', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-            {title: '3학년', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-            {title: '4학년', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+            {title: '1학년', data: ['SW에듀서포터즈 활동', 'SW English 수강']},
+            {title: '2학년', data: ['SW특허출원', '교내 Programming GURU 수강', 'SW설계 및 개발', 'SW특강 참여', 'TOPCIT응시']},
+            {title: '3학년', data: ['코딩 자격증 응시', 'SW중심대학 사업단 주관 행사 참여', 'SW컨퍼런스 참여', '해외 장기 현장실습', 'SW창업교육']},
+            {title: '4학년', data: ['국내 학술 발표대회 우수상 수상', 'SW분야 취업 특강', 'SW 창업 경진대회 참가', 'SW소학회', '에듀 서포터즈 활동']},
           ]}
           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
